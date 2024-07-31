ef81d71884e8e52185e3f2c2117199a1f1a4e6ec
Matic Babnik <matic.babnik@outlook.com>
2024-07-31 23:04:43 +0200

    fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,19 @@
-interface Person {
-    slug: string;
-    url: string;
-    name: strin;
-}
+declare module "wikifeet-js" {
 
-interface Image {
-    id: number;
-    thumb: string;
-    image: string;
-    resolution: [number, number];
-    tags: string[];
+    export interface Person {
+        slug: string;
+        url: string;
+        name: string;
+    }
+    
+    export interface Image {
+        id: number;
+        thumb: string;
+        image: string;
+        resolution: [number, number];
+        tags: string[];
+    }
+    
+    export function search(query: string): Promise<Person[]>;
+    export function page(person: Person): Promise<{ images: Image[]; tags: string[] }>;
 }
-
-function search(query: string): Promise<Person[]>;
-function page(person: Person): Promise<{ images: Image[]; tags: string[] }>;
