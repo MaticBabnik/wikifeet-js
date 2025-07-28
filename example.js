a7c5908536fd4476789bfc84dd5eaa37af32b679
Martin Hanzlowsky <martin.hanzlowskyg@gmail.com>
2025-07-29 00:07:44 +0200

    Handle male content.

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 import * as wikifeet from "./index.js";
 
-const pokimane = (await wikifeet.search("pokimane"))[0];
+const pokimane = (await wikifeet.search("hasan piker"))[0];
 console.log({ pokimane });
 
 const page = await wikifeet.page(pokimane);
